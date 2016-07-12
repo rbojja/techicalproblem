9b4fdbc12f2600b875e5b995948027c26ceb4b9e
rbojja <rajesh.bojja@cloudwick.com>
2016-07-12 10:59:54 -0700

    fixes

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-# Name:        module2_testing
+# Name:        Script to count number of rejects per source ip and uploading to aws s3 bucket 
 # Purpose:
 #
 # Author:      Rajesh
@@ -20,13 +20,17 @@ def getCountRejects(file_path):
 
         with open(file_path) as f_handle:
             for line in f_handle:
+#Finding REJECT in each line and then using split function to store the sourceip address which is field srcaddress after interface-id field 
                 if 'REJECT' in line:
                     temp=line.split()
                     sourceip=str(temp[4])
                     if sourceip in string_dict:
+#If sourceip already exists in dictionary ,incrementing the number of rejects for this specific sourceip
                         string_dict[sourceip] += 1
                     else:
+#If sourceip does not exist in dictionary key ,then creating key and initializing the value to 1 
                         string_dict[sourceip] = 1
+#Converting dictionary to json format without creating temporary file in local folder
             json_string = json.dumps(string_dict)
 
         return json_string
@@ -38,31 +42,38 @@ def getCountRejects(file_path):
 def uploadingtos3(json_formatted_string,bucket_key,bucket_name):
     try:
 
-        # Upload the file to S3
-
+# Upload the file to S3
+#Establishing connection assuming access keys as environment variables otherwise need to pass access key and secret access key as parameters to the function
         c = boto.connect_s3()
         b = c.get_bucket(bucket_name)
+#Getting bucket name from the s3 url given and assigning key 
         k=Key(b)
         k.key=bucket_key
+#Storing the json formatted result to the s3 key 
         k.set_contents_from_string(json_formatted_string)
 
     except:
         print("Error in uploading file")
 
 
+#main function
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+#Initializing dictionary 
     string_dict={}
+#Getting arguments and storing into file_path and s3_path variables
     file_path=sys.argv[1]
     s3_path=sys.argv[2]
-    #Assuming the file path as s3://dwollatechnicalexercise/problem1 where bucketname =dwollatechnicalexercise key= problem1
+    #Assuming the file path format as s3://dwollatechnicalexercise/problem1 where bucketname =dwollatechnicalexercise key= problem1
     name=str(s3_path).split("/")
     bucket_name= name[2]
     bucket_key=name[3]
+#counting number of rejects per source ip by calling function getCountRejects which returns the json result 
     json_formatted_string=getCountRejects(file_path)
+#uploading to s3 bucket
     uploadingtos3(json_formatted_string,bucket_key,bucket_name)
 
 
